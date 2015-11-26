8aff0bcbcf0aa345f9adf1efd395200cafdd6b99
nindianpd <nindi.siiplah@gmail.com>
2015-11-26 23:29:25 +0700

    commit lokal-pertama

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
 
 echo "<h1> Selamat Datang Di Belajar GitHub</h1>";
+echo "<br> klik disini untuk masuk kehalaman utama";
 ?>
---
+++ utama.php
@@ -0,0 +1,3 @@
+<?php
+ echo "<h1> Halloooo ini halaman utamanya baru dibuat</h1>";
+?>
